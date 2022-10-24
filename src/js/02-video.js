470983bc3e3d756bfa7803fb4a6f7627ca014957
Naliv88 <nalivpv@gmail.com>
2022-10-24 10:14:30 +0300

    update

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,34 @@
 
+import Player from '@vimeo/player';
+
+
+const iframe = document.querySelector('#vimeo-player');
+
+const options = {
+    width: 640,
+    muted: true,
+}
+const player = new Player(iframe, options);
+
+    player.on('play', function() {
+        console.log('played the video!');
+    });
+
+    // const onPlay = function(data) {
+    // // data is an object containing properties specific to that event
+    // };
+
+    // player.on('play', onPlay);
+
+    // // If later on you decide that you don’t need to listen for play anymore.
+    player.pause('play', function() {
+        console.log('stop the video!');
+    });
+    
+
+    player.getVideoTitle().then(function(title) {
+        console.log('title:', title);
+    });
+
+console.log(iframe);
+console.log(player);
